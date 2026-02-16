d572559041081170a74a23cba7d8279bf7b51062
Peralta24 <rafamtz301203@gmail.com>
2026-02-16 07:29:43 -0600

    Initial commit

---
+++ UltimateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltimateApp
+//
+//  Created by Jose Rafael Peralta Martinez  on 16/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltimateApp/UltimateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateAppApp.swift
+//  UltimateApp
+//
+//  Created by Jose Rafael Peralta Martinez  on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct UltimateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
