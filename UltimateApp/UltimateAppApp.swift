796639ea5c211e355428cc5c4b810d59b809a175
Peralta24 <rafamtz301203@gmail.com>
2026-07-01 05:55:37 -0600

    Initial commit

---
+++ UltimateApp/UltimateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateAppApp.swift
+//  UltimateApp
+//
+//  Created by Jose Rafael Peralta Martinez  on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UltimateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
