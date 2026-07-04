3a774ebb483f8032ddbff72ab4255c0aea66d555
José Rafael Peralta Martínez <rafamtz301203@gmail.com>
2026-07-03 18:58:27 -0600

    Agregando numero a Issue

--- UltimateApp/ContentView.swift
+++ UltimateApp/ContentView.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+internal import CoreData
 
 struct ContentView: View {
+     @EnvironmentObject var dataController: DataController
+    
+    var issue: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+        
+        if let tag = filter.tag {
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []
+        } else {
+            let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+        
+        return allIssues.sorted()
+    }
     var body: some View {
-       Text("Content")
+        List {
+            ForEach(issue) { issue in
+                Text(issue.issueTitle)
+            }
+        }
+        .navigationTitle("Issue")
     }
 }
 
